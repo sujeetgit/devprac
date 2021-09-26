882343bffacfb84e7291923a99131f90ffa0296f
sujeetgit <sujeet.srivastava@gmail.com>
2021-09-26 14:17:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  " + lname + " " + fname)
+y=len(fname)+len(lname)
+print ("Your name has" + str(y) + "characters")
